5fa5d5d0d8bdbe9b67464e24e9ee6de5a08e8235
Sagar Ekbote <sagar@sagar.local>
2020-06-25 17:11:20 +0530

    added space

--- app/src/main/java/com/demo/news/NewsApplication.java
+++ app/src/main/java/com/demo/news/NewsApplication.java
@@ -7,7 +7,7 @@ import com.demo.news.di.component.ApplicationComponent;
 import com.demo.news.di.component.DaggerApplicationComponent;
 import com.demo.news.di.module.ApplicationModule;
 
-public class NewsApplication extends Application {
+public class NewsApplication extends Application  {
 
     protected ApplicationComponent mApplicationComponent;
 
